f5230846ff2526efaf3eb437e925eb2c761bf774
loquens02 <loquens20@hanyang.ac.kr>
2021-07-07 05:58:46 +0900

    mainly[Tree-Insert 구현 genera case_random int inputs], modCode[TreeNode-VO 기본형만 남기고 나머지 없앰_T key, TreeProgram-generous insertNode_return treeRoot]

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,57 +1,20 @@
-import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
-
-public class TreeNode<T>{//<T extends Integer>{// implements Comparable<Number>{
+public class TreeNode<T extends Number>{
 //    public T parent;
-    int key;
-    public TreeNode<T> left;
-    public TreeNode<T> right;
+    T key;
+    TreeNode<T> left;
+    TreeNode<T> right;
 
-    public TreeNode(int key) {
+    public TreeNode(T key) {
         this.key = key;
     }
 
-    //    새 노드 생성
-    public TreeNode<T> makeNode(int  key){
-        return new TreeNode<>(key);
-    }
+//    public TreeNode() { }
 
-
-
-//    기존 노드가 있다면. 거기에 추가하고 싶다면.
-    public TreeNode<T> insertNode(int  key){
-        TreeNode<T> temp;
-        TreeNode<T> newNode= makeNode(key);
-
-//          '왼쪽자식 < 부모_node <= 오른쪽자식' 되도록
-        if (smallerThanParent(key)){
-            if(null == left){
-                left= newNode;
-            }
-            temp= left;
-        } else { // biggerOrSame ThanParent
-            if(null == right){
-                right= newNode;
-            }
-            temp= right;
-        }
-
-        return this;    // Chaining - https://stackoverflow.com/a/21180314
-    }
-
-    // this.key: 현재 노드. parent
-    // key: 넣을 값
-    boolean smallerThanParent(int key){
-        return this.key > key ? TRUE : FALSE;
-    }
-
-
-
-    public int getKey() {
+    public T getKey() {
         return key;
     }
 
-    public void setKey(int key) {
+    public void setKey(T key) {
         this.key = key;
     }
 
--- src/TreeProgram.java
+++ src/TreeProgram.java
@@ -1,21 +1,33 @@
-
 public class TreeProgram {
 
     public static void main(String[] args) {
-        TreeNode node= treeBuilder(11);
+        // 만약 첫 노드가 1이면 끔찍 => 이진탐색트리에서 배분해줘야.
 
-//        simple test case
-        node.insertNode(20).insertNode(8);
-//        node.insertNode(8).insertNode(20);
+//        미할당 시 객체 초깃값이 null 이 아니더라.
+        TreeNode node = null;
 
-        System.out.println(String.valueOf(node.getLeft().getKey()));
-        System.out.println(String.valueOf(node.getRight().getKey()));
+        node= insertNode(node, makeNode(11));
+        node= insertNode(node, makeNode(15));
+        node= insertNode(node, makeNode(9));
+
+        int count= 5;
+        while(count > 0){
+            int tempKey= (int)(Math.random()*100);
+            System.out.println(tempKey);
+
+            node= insertNode(node, makeNode(tempKey));
+            count--;
+        }
+
+//        beautify java object. https://codebeautify.org/javaviewer
+//        System.out.println(String.valueOf(node.getLeft().getKey()));
+        System.out.println(String.valueOf(node));
 
 
-        System.out.println(String.valueOf(treeSearch(node,8).getKey()));
+        System.out.println(String.valueOf(treeSearch(node,9).getKey()));
     }
 
-    static TreeNode treeBuilder(int key){
+    static TreeNode<Integer> makeNode(int key){
         return new TreeNode(key);
     }
 
@@ -30,6 +42,36 @@ public class TreeProgram {
         } else {
             return treeSearch(node.getRight(), key);
         }
+    }
+
+//    treeRoot not nullable - null? NullPointerException!
+    static public TreeNode<Integer> insertNode(TreeNode<Integer> treeRoot, TreeNode<Integer> inputNode){
+        TreeNode<Integer> lastHead= null;
+        TreeNode<Integer> head= treeRoot;    // 서브트리의 머리. 처음에는 root. while 끝나면 null
+
+        while (head != null){
+            lastHead= head;         // 마지막으로 보던 서브트리의 머리
+            if (inputNode.getKey() < head.getKey()){
+                head= head.left;
+            } else{
+                head= head.right;
+            }
+        }
+
+//        parent 멤버 불필요. 이거 없이도 treeRoot 밑에 lastHead 가 연결이 된다!!
+//        inputNode.parent= lastHead;
+
+//        처음에 빈 트리였을 때. treeRoot== null.
+        if (lastHead==null) {
+            treeRoot= inputNode;
+        }
+        else if (inputNode.getKey() < lastHead.getKey()){
+            lastHead.left= inputNode;
+        } else {
+            lastHead.right= inputNode;
+        }
+
+        return treeRoot;
     }
 }
 
